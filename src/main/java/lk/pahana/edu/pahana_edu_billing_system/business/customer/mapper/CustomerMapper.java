f4670adaf07c86c551d1936ff537ef8de5114df2
Vimu05 <123923720+Vimu05@users.noreply.github.com>
2025-08-03 15:11:49 +0530

    add customer mapper

---
+++ src/main/java/lk/pahana/edu/pahana_edu_billing_system/business/customer/mapper/CustomerMapper.java
@@ -0,0 +1,46 @@
+package lk.pahana.edu.pahana_edu_billing_system.business.customer.mapper;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lk.pahana.edu.pahana_edu_billing_system.business.customer.dto.CustomerDTO;
+import lk.pahana.edu.pahana_edu_billing_system.business.customer.model.Customer;
+
+
+public class CustomerMapper {
+
+    public static CustomerDTO toDTO(Customer customer) {
+        if (customer == null) return null;
+
+        return new CustomerDTO.Builder()
+                .customerId(customer.getCustomerId())
+                .name(customer.getName())
+                .address(customer.getAddress())
+                .mobileNumber(customer.getMobileNumber())
+                .unitsConsumed(customer.getUnitsConsumed())
+                .registrationDate(customer.getRegistrationDate())
+                .email(customer.getEmail())
+                .build();
+    }
+
+    public static Customer toEntity(CustomerDTO dto) {
+        if (dto == null) return null;
+
+        return new Customer.Builder()
+                .customerId(dto.getCustomerId())
+                .name(dto.getName())
+                .address(dto.getAddress())
+                .mobileNumber(dto.getMobileNumber())
+                .unitsConsumed(dto.getUnitsConsumed())
+                .registrationDate(dto.getRegistrationDate())
+                .email(dto.getEmail())
+                .build();
+    }
+
+    public static CustomerDTO buildCustomerDTOFromRequest(HttpServletRequest req) {
+        return new CustomerDTO.Builder()
+                .name(req.getParameter("name"))
+                .address(req.getParameter("address"))
+                .mobileNumber(req.getParameter("mobileNumber"))
+                .email(req.getParameter("email"))
+                .build();
+    }
+}
