adc77fab49de31db986ed4303cd92635ca79e6fd
Vimu05 <123923720+Vimu05@users.noreply.github.com>
2025-08-03 23:32:20 +0530

    add get customer to customer servlet

---
+++ src/main/java/lk/pahana/edu/pahana_edu_billing_system/business/customer/servlet/EditCustomerServlet.java
@@ -0,0 +1,38 @@
+package lk.pahana.edu.pahana_edu_billing_system.business.customer.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lk.pahana.edu.pahana_edu_billing_system.business.customer.dto.CustomerDTO;
+import lk.pahana.edu.pahana_edu_billing_system.business.customer.mapper.CustomerMapper;
+import lk.pahana.edu.pahana_edu_billing_system.business.customer.service.CustomerService;
+import lk.pahana.edu.pahana_edu_billing_system.business.customer.service.impl.CustomerServiceImpl;
+
+import java.io.IOException;
+
+@WebServlet(name = "edit-customer", urlPatterns = "/customer/edit")
+public class EditCustomerServlet extends HttpServlet {
+
+    private CustomerService customerService;
+
+    @Override
+    public void init() {
+        customerService = new CustomerServiceImpl();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String id = req.getParameter("id");
+
+        CustomerDTO customer = customerService.getCustomerById(id);
+
+        req.setAttribute("customer", customer);
+        req.setAttribute("pageTitle", "Edit Customer");
+        req.setAttribute("body", "../customer/edit-customer.jsp");
+
+        req.getRequestDispatcher("/WEB-INF/views/layout/layout.jsp").forward(req, resp);
+    }
+
+}
