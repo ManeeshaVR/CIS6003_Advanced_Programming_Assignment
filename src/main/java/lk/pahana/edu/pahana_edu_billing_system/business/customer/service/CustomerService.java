d52642030fd018ae8ce4e953676c557e0584d01f
Vimu05 <123923720+Vimu05@users.noreply.github.com>
2025-08-03 15:13:47 +0530

    add customer service

---
+++ src/main/java/lk/pahana/edu/pahana_edu_billing_system/business/customer/service/CustomerService.java
@@ -0,0 +1,9 @@
+package lk.pahana.edu.pahana_edu_billing_system.business.customer.service;
+
+import lk.pahana.edu.pahana_edu_billing_system.business.customer.dto.CustomerDTO;
+
+public interface CustomerService {
+
+    void saveCustomer(CustomerDTO customer);
+
+}
