fff5ff2048eaa6ce2c13e83dd40830b41d282d22
Vimu05 <123923720+Vimu05@users.noreply.github.com>
2025-08-06 08:38:42 +0530

    add create method to item servlet

--- src/main/java/lk/pahana/edu/pahana_edu_billing_system/business/item/servlet/ItemServlet.java
+++ src/main/java/lk/pahana/edu/pahana_edu_billing_system/business/item/servlet/ItemServlet.java
@@ -6,11 +6,14 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lk.pahana.edu.pahana_edu_billing_system.business.item.dto.ItemDTO;
+import lk.pahana.edu.pahana_edu_billing_system.business.item.mapper.ItemMapper;
 import lk.pahana.edu.pahana_edu_billing_system.business.item.service.ItemService;
 import lk.pahana.edu.pahana_edu_billing_system.business.item.service.impl.ItemServiceImpl;
 
 import java.io.IOException;
 import java.util.List;
+
+import static lk.pahana.edu.pahana_edu_billing_system.util.validation.Validation.validateItemDTO;
 
 @WebServlet(name = "item", urlPatterns = "/item")
 public class ItemServlet extends HttpServlet {
@@ -31,6 +34,21 @@ public class ItemServlet extends HttpServlet {
         req.setAttribute("body", "../item/view-item.jsp");
 
         req.getRequestDispatcher("/WEB-INF/views/layout/layout.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ItemDTO itemDTO = ItemMapper.buildItemDTOFromRequest(req);
+        String validationError = validateItemDTO(itemDTO);
+
+        if (validationError == null) {
+            itemService.saveItem(itemDTO);
+            req.getSession().setAttribute("flash_success", "Item created successfully!");
+        } else {
+            req.getSession().setAttribute("flash_error", validationError);
+        }
+
+        resp.sendRedirect(req.getContextPath() + "/item");
     }
 
 }
