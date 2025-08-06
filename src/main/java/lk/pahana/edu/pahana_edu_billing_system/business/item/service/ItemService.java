f277dfe597aa77673f293caa3e9fece75134eb93
Vimu05 <123923720+Vimu05@users.noreply.github.com>
2025-08-06 08:21:08 +0530

    add get all items method to service

---
+++ src/main/java/lk/pahana/edu/pahana_edu_billing_system/business/item/service/ItemService.java
@@ -0,0 +1,12 @@
+package lk.pahana.edu.pahana_edu_billing_system.business.item.service;
+
+import lk.pahana.edu.pahana_edu_billing_system.business.item.dto.ItemDTO;
+
+import java.util.List;
+
+public interface ItemService {
+
+    List<ItemDTO> getAllItems();
+
+
+}
---
+++ src/main/java/lk/pahana/edu/pahana_edu_billing_system/business/item/service/impl/ItemServiceImpl.java
@@ -0,0 +1,27 @@
+package lk.pahana.edu.pahana_edu_billing_system.business.item.service.impl;
+
+import lk.pahana.edu.pahana_edu_billing_system.business.item.dto.ItemDTO;
+import lk.pahana.edu.pahana_edu_billing_system.business.item.mapper.ItemMapper;
+import lk.pahana.edu.pahana_edu_billing_system.business.item.model.Item;
+import lk.pahana.edu.pahana_edu_billing_system.business.item.service.ItemService;
+import lk.pahana.edu.pahana_edu_billing_system.persistence.item.dao.ItemDAO;
+import lk.pahana.edu.pahana_edu_billing_system.persistence.item.dao.impl.ItemDAOImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ItemServiceImpl implements ItemService {
+
+    ItemDAO itemDAO = new ItemDAOImpl();
+
+    @Override
+    public List<ItemDTO> getAllItems() {
+        List<ItemDTO> itemDTOS = new ArrayList<>();
+        List<Item> itemList = itemDAO.findAll();
+        for (Item item : itemList) {
+            itemDTOS.add(ItemMapper.toDTO(item));
+        }
+        return itemDTOS;
+    }
+
+}
