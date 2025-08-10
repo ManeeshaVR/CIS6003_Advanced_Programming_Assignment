e1d5bb69903e65a0df9720ae1e5c19b56633aaf3
Vimu05 <123923720+Vimu05@users.noreply.github.com>
2025-08-10 23:20:13 +0530

    add order sql queries

--- src/main/java/lk/pahana/edu/pahana_edu_billing_system/util/db/SqlQueries.java
+++ src/main/java/lk/pahana/edu/pahana_edu_billing_system/util/db/SqlQueries.java
@@ -36,4 +36,32 @@ public class SqlQueries {
                 "DELETE FROM item WHERE item_code = ?";
     }
 
+    public static final class Order {
+        public static final String INSERT =
+                "INSERT INTO orders (order_id, order_date, customer_id, total_amount) VALUES (?, ?, ?, ?)";
+
+        public static final String FIND_LAST =
+                "SELECT \n" +
+                        "    o.order_id,\n" +
+                        "    o.order_date,\n" +
+                        "    o.customer_id,\n" +
+                        "    o.total_amount,\n" +
+                        "    oi.item_code,\n" +
+                        "    oi.quantity,\n" +
+                        "    oi.unit_price\n" +
+                        "FROM orders o\n" +
+                        "JOIN order_item oi ON o.order_id = oi.order_id\n" +
+                        "WHERE o.order_id = (\n" +
+                        "    SELECT order_id \n" +
+                        "    FROM orders \n" +
+                        "    ORDER BY order_date DESC, order_id DESC \n" +
+                        "    LIMIT 1\n" +
+                        ");\n";
+    }
+
+    public static final class OrderItem {
+        public static final String INSERT =
+                "INSERT INTO order_item (order_id, item_code, quantity, unit_price) VALUES (?, ?, ?, ?)";
+    }
+
 }
